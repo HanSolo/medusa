fbbf36f0792d5c9ee1aff8a7307b314d82b21f06
Gerrit Grunwald <han.solo@mac.com>
2016-01-13 13:17:25 +0100

    Added a FGaugeBuilder class

---
+++ src/main/java/eu/hansolo/medusa/FGaugeBuilder.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.medusa;
+
+import eu.hansolo.medusa.GaugeDesign.GaugeBackground;
+import eu.hansolo.medusa.Marker.MarkerEvent;
+import eu.hansolo.medusa.Marker.MarkerType;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.Dimension2D;
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+
+
+/**
+ * Created by hansolo on 13.01.16.
+ */
+public class FGaugeBuilder<B extends FGaugeBuilder<B>> {
+    private HashMap<String, Property> properties = new HashMap<>();
+
+
+    // ******************** Constructors **************************************
+    protected FGaugeBuilder() {}
+
+
+    // ******************** Methods *******************************************
+    public static final FGaugeBuilder create() {
+        return new FGaugeBuilder();
+    }
+
+    public final B gauge(final Gauge GAUGE) {
+        properties.put("gauge", new SimpleObjectProperty<>(GAUGE));
+        return (B)this;
+    }
+
+    public final B gaugeDesign(final GaugeDesign DESIGN) {
+        properties.put("gaugeDesign", new SimpleObjectProperty<>(DESIGN));
+        return (B)this;
+    }
+
+    public final B gaugeBackground(final GaugeBackground BACKGROUND) {
+        properties.put("gaugeBackground", new SimpleObjectProperty<>(BACKGROUND));
+        return (B)this;
+    }
+
+    public final B foregroundVisible(final boolean VISIBLE) {
+        properties.put("foregroundVisible", new SimpleBooleanProperty(VISIBLE));
+        return (B)this;
+    }
+
+    public final B prefSize(final double WIDTH, final double HEIGHT) {
+        properties.put("prefSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B)this;
+    }
+    public final B minSize(final double WIDTH, final double HEIGHT) {
+        properties.put("minSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B)this;
+    }
+    public final B maxSize(final double WIDTH, final double HEIGHT) {
+        properties.put("maxSize", new SimpleObjectProperty<>(new Dimension2D(WIDTH, HEIGHT)));
+        return (B)this;
+    }
+
+    public final B prefWidth(final double PREF_WIDTH) {
+        properties.put("prefWidth", new SimpleDoubleProperty(PREF_WIDTH));
+        return (B)this;
+    }
+    public final B prefHeight(final double PREF_HEIGHT) {
+        properties.put("prefHeight", new SimpleDoubleProperty(PREF_HEIGHT));
+        return (B)this;
+    }
+
+    public final B minWidth(final double MIN_WIDTH) {
+        properties.put("minWidth", new SimpleDoubleProperty(MIN_WIDTH));
+        return (B)this;
+    }
+    public final B minHeight(final double MIN_HEIGHT) {
+        properties.put("minHeight", new SimpleDoubleProperty(MIN_HEIGHT));
+        return (B)this;
+    }
+
+    public final B maxWidth(final double MAX_WIDTH) {
+        properties.put("maxWidth", new SimpleDoubleProperty(MAX_WIDTH));
+        return (B)this;
+    }
+    public final B maxHeight(final double MAX_HEIGHT) {
+        properties.put("maxHeight", new SimpleDoubleProperty(MAX_HEIGHT));
+        return (B)this;
+    }
+
+    public final B scaleX(final double SCALE_X) {
+        properties.put("scaleX", new SimpleDoubleProperty(SCALE_X));
+        return (B)this;
+    }
+    public final B scaleY(final double SCALE_Y) {
+        properties.put("scaleY", new SimpleDoubleProperty(SCALE_Y));
+        return (B)this;
+    }
+
+    public final B layoutX(final double LAYOUT_X) {
+        properties.put("layoutX", new SimpleDoubleProperty(LAYOUT_X));
+        return (B)this;
+    }
+    public final B layoutY(final double LAYOUT_Y) {
+        properties.put("layoutY", new SimpleDoubleProperty(LAYOUT_Y));
+        return (B)this;
+    }
+
+    public final B translateX(final double TRANSLATE_X) {
+        properties.put("translateX", new SimpleDoubleProperty(TRANSLATE_X));
+        return (B)this;
+    }
+    public final B translateY(final double TRANSLATE_Y) {
+        properties.put("translateY", new SimpleDoubleProperty(TRANSLATE_Y));
+        return (B)this;
+    }
+
+    public final FGauge build() {
+        Gauge           gauge      = null == properties.get("gauge") ? new Gauge() : ((ObjectProperty<Gauge>) properties.get("gauge")).get();
+        GaugeDesign     design     = null == properties.get("gaugeDesign") ? GaugeDesign.METAL : ((ObjectProperty<GaugeDesign>) properties.get("gaugeDesign")).get();
+        GaugeBackground background = null == properties.get("gaugeBackground") ? GaugeBackground.DARK_GRAY : ((ObjectProperty<GaugeBackground>) properties.get("gaugeBackground")).get();
+
+        final FGauge CONTROL = new FGauge(gauge, design, background);
+        for (String key : properties.keySet()) {
+            if ("prefSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setPrefSize(dim.getWidth(), dim.getHeight());
+            } else if("minSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setPrefSize(dim.getWidth(), dim.getHeight());
+            } else if("maxSize".equals(key)) {
+                Dimension2D dim = ((ObjectProperty<Dimension2D>) properties.get(key)).get();
+                CONTROL.setPrefSize(dim.getWidth(), dim.getHeight());
+            } else if("prefWidth".equals(key)) {
+                CONTROL.setPrefWidth(((DoubleProperty) properties.get(key)).get());
+            } else if("prefHeight".equals(key)) {
+                CONTROL.setPrefHeight(((DoubleProperty) properties.get(key)).get());
+            } else if("minWidth".equals(key)) {
+                CONTROL.setMinWidth(((DoubleProperty) properties.get(key)).get());
+            } else if("minHeight".equals(key)) {
+                CONTROL.setMinHeight(((DoubleProperty) properties.get(key)).get());
+            } else if("maxWidth".equals(key)) {
+                CONTROL.setMaxWidth(((DoubleProperty) properties.get(key)).get());
+            } else if("maxHeight".equals(key)) {
+                CONTROL.setMaxHeight(((DoubleProperty) properties.get(key)).get());
+            } else if("scaleX".equals(key)) {
+                CONTROL.setScaleX(((DoubleProperty) properties.get(key)).get());
+            } else if("scaleY".equals(key)) {
+                CONTROL.setScaleY(((DoubleProperty) properties.get(key)).get());
+            } else if ("layoutX".equals(key)) {
+                CONTROL.setLayoutX(((DoubleProperty) properties.get(key)).get());
+            } else if ("layoutY".equals(key)) {
+                CONTROL.setLayoutY(((DoubleProperty) properties.get(key)).get());
+            } else if ("translateX".equals(key)) {
+                CONTROL.setTranslateX(((DoubleProperty) properties.get(key)).get());
+            } else if ("translateY".equals(key)) {
+                CONTROL.setTranslateY(((DoubleProperty) properties.get(key)).get());
+            } else if ("foregroundVisible".equals(key)) {
+                CONTROL.setForegroundVisible(((BooleanProperty) properties.get(key)).get());
+            }
+        }
+        return CONTROL;
+    }
+}
