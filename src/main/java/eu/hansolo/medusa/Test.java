43cfd0b19307587acb0d24dd2417e94c96800cfd
Gerrit Grunwald <han.solo@mac.com>
2016-01-28 17:11:03 +0100

    Just some tests with new Clock control

--- src/main/java/eu/hansolo/medusa/Test.java
+++ src/main/java/eu/hansolo/medusa/Test.java
@@ -16,20 +16,20 @@
 
 package eu.hansolo.medusa;
 
+import eu.hansolo.medusa.Alarm.Repetition;
+import eu.hansolo.medusa.Clock.ClockSkinType;
 import eu.hansolo.medusa.Gauge.ButtonEvent;
 import eu.hansolo.medusa.Gauge.KnobType;
-import eu.hansolo.medusa.Gauge.LcdFont;
 import eu.hansolo.medusa.Gauge.LedType;
 import eu.hansolo.medusa.Gauge.NeedleShape;
 import eu.hansolo.medusa.Gauge.NeedleSize;
 import eu.hansolo.medusa.Gauge.ScaleDirection;
 import eu.hansolo.medusa.Gauge.SkinType;
 import eu.hansolo.medusa.Gauge.ThresholdEvent;
-import eu.hansolo.medusa.Gauge.TickLabelLocation;
-import eu.hansolo.medusa.Gauge.TickLabelOrientation;
-import eu.hansolo.medusa.Gauge.TickMarkType;
 import eu.hansolo.medusa.GaugeDesign.GaugeBackground;
 import eu.hansolo.medusa.Marker.MarkerType;
+import eu.hansolo.medusa.events.AlarmEvent;
+import eu.hansolo.medusa.events.AlarmEventListener;
 import eu.hansolo.medusa.skins.*;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -48,6 +48,7 @@ import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
@@ -57,6 +58,9 @@ import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Locale;
 import java.util.Random;
 
 
@@ -70,11 +74,44 @@ public class Test extends Application {
     private static final Random         RND = new Random();
     private static       int            noOfNodes = 0;
     private              Gauge          gauge;
+    private              Clock          clock;
     private              long           lastTimerCall;
     private              AnimationTimer timer;
 
     @Override public void init() {
         gauge = GaugeBuilder.create()
+                            .skinType(SkinType.LCD)
+                            .prefSize(400, 200)
+                            .title("Münster")
+                            .lcdDesign(LcdDesign.STANDARD_GREEN)
+                            .build();
+
+        class Command1 implements Command {
+            @Override public void execute() {
+                System.out.println("Command in other class executed");
+            }
+        }
+        Command1 command1 = new Command1();
+
+
+        clock = ClockBuilder.create()
+                            .skinType(ClockSkinType.LCD)
+                            .prefSize(400, 200)
+                            .lcdFont(LcdFont.DIGITAL_BOLD)
+                            //.time(LocalDateTime.now(ZoneId.of("America/New_York")))
+                            .dateVisible(true)
+                            .locale(Locale.GERMAN)
+                            .lcdDesign(LcdDesign.STANDARD_GREEN)
+                            .title("Münster")
+                            .titleVisible(true)
+                            .secondsVisible(true)
+                            .textVisible(true)
+                            .discreteSteps(true)
+                            .alarms(new Alarm(Repetition.ONCE, LocalDateTime.now().plusSeconds(5), Alarm.ARMED, "5 sec after Start"),
+                                    new Alarm(Repetition.ONCE, LocalDateTime.now().plusSeconds(10), Alarm.ARMED, "10 sec after Start", command1))
+                            .alarmsEnabled(false)
+                            .onAlarm(alarmEvent -> System.out.println(alarmEvent.ALARM.getText()))
+                            .running(true)
                             .build();
 
         lastTimerCall = System.nanoTime();
@@ -89,7 +126,8 @@ public class Test extends Application {
     }
 
     @Override public void start(Stage stage) {
-        StackPane pane = new StackPane(gauge);
+        HBox pane = new HBox(clock, gauge);
+        pane.setSpacing(20);
         pane.setPadding(new Insets(10));
         LinearGradient gradient = new LinearGradient(0, 0, 0, pane.getLayoutBounds().getHeight(),
                                                      false, CycleMethod.NO_CYCLE,
@@ -108,7 +146,7 @@ public class Test extends Application {
         //gauge.setValue(0.35);
 
         // Calculate number of nodes
-        calcNoOfNodes(gauge);
+        calcNoOfNodes(pane);
         System.out.println(noOfNodes + " Nodes in SceneGraph");
 
         timer.start();
