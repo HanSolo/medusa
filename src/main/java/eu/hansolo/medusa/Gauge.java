0d24b9b1ac87eb8e76aa279bfac236d5ead63deb
Gerrit Grunwald <han.solo@mac.com>
2016-12-14 13:39:49 +0100

    Ensure that the NodeOrientation is always LEFT_TO_RIGHT for the gauge

--- src/main/java/eu/hansolo/medusa/Gauge.java
+++ src/main/java/eu/hansolo/medusa/Gauge.java
@@ -49,6 +49,7 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.event.EventTarget;
 import javafx.event.EventType;
+import javafx.geometry.NodeOrientation;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.Control;
@@ -400,6 +401,7 @@ public class Gauge extends Control {
         this(SkinType.GAUGE);
     }
     public Gauge(final SkinType SKIN) {
+        setNodeOrientation(NodeOrientation.LEFT_TO_RIGHT);
         skinType = SKIN;
         getStyleClass().add("gauge");
 
